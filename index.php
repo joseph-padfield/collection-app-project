ce3ac5ecdc7b4b780baa05d418f4a3bb2bdfb021
Joseph Padfield <joseph.padfield@gmail.com>
2024-07-29 15:16:20 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="style.css" rel="stylesheet">
+    <title>Films Collector</title>
+</head>
+
+<body>
+
+<header>
+
+</header>
+
+<main>
+
+</main>
+
+<footer>
+
+</footer>
+
+</body>
+
+
+</html>
+
+
+
+
+
+<?php
+
+$db = new PDO('mysql:host=db; dbname=my_films', 'root', 'password');
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare('SELECT * FROM `films`');
+$query->execute();
+
+$results = $query->fetchAll();
+
+var_dump($results);
+
+?>
+
