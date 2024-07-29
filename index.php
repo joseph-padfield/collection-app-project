97d91e9860d0a0f8a83add9f3874bdf930fc0328
Joseph Padfield <joseph.padfield@gmail.com>
2024-07-29 15:51:41 +0100

    php file containing code connecting to db and returning results. html and css for basic styling in order to see two items per line.

--- index.php
+++ index.php
@@ -11,10 +11,37 @@
 <body>
 
 <header>
-
+<div class="banner">
+    <div>
+        <h1>Film Collector App</h1>
+    </div>
+</div>
 </header>
 
 <main>
+    <div class="main_container">
+        <?php
+
+        $db = new PDO('mysql:host=db; dbname=my_films', 'root', 'password');
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        $query = $db->prepare('SELECT * FROM `films`');
+        $query->execute();
+
+        $results = $query->fetchAll();
+
+        foreach ($results as $film){
+            echo '<div class="item">';
+            echo '<p>' . $film['Title'] . '</p>';
+            echo '<p>' . $film['Director'] . '</p>';
+            echo '<p>' . $film['Year'] . '</p>';
+            echo '</div>';
+        }
+        ?>
+    </div>
+
 
 </main>
 
@@ -31,19 +58,5 @@
 
 
 
-<?php
 
-$db = new PDO('mysql:host=db; dbname=my_films', 'root', 'password');
-
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-$query = $db->prepare('SELECT * FROM `films`');
-$query->execute();
-
-$results = $query->fetchAll();
-
-var_dump($results);
-
-?>
 
