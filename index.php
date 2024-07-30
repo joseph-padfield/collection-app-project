a68092fbc9ea82eef2fa1f3c60ac08898cdf3012
Joseph Padfield <joseph.padfield@gmail.com>
2024-07-30 15:00:36 +0100

    Created form, set input types, validates data before sending to DB.

--- index.php
+++ index.php
@@ -12,8 +12,11 @@
 
 <header>
 <div class="banner">
-    <div>
+    <div class="banner_left">
         <h1><span class="left_float">Film</span><br><span>Collector</span><br><span class="right_float">App</span></h1>
+    </div>
+    <div class="banner_right">
+
     </div>
 </div>
 </header>
@@ -32,6 +35,63 @@
             echo '<p>' . $film['Director'] . '</p>';
             echo '<p>' . $film['Year'] . '</p>';
             echo '</div>';
+        }
+        ?>
+    </div>
+    <div class="add_new_item">
+        <?
+        echo "<form method='post'>";
+
+        echo "<label for='title'>Title  </label>";
+        echo "<input id='title' name='title' type='text' placeholder='Film Title'>";
+
+        echo "<label for='runtime'>Runtime (mins)  </label>";
+        echo "<input id='runtime' name='runtime' type='number' placeholder='Runtime'>";
+
+        echo "<label for='year'>Year released  </label>";
+        echo "<input id='year' name='year' type='number' placeholder='Release year' maxlength='4'>";
+
+        echo "<label for='director'>Director  </label>";
+        echo "<input id='director' name='director' type='text' placeholder='Director'>";
+
+        echo "<label for='country'>Produced (country)  </label>";
+        echo "<input id='country' name='country' type='text' placeholder='Produced (country)'>";
+
+        echo "<label for='language'>Language  </label>";
+        echo "<input id='language' name='language' type='text' placeholder='Language'>";
+
+        echo "<label for='image_url'>Image URL  </label>";
+        echo "<input id='image_url' name='image_url' type='text' placeholder='url'>";
+
+        echo "<input id='submit' name='submit' type='submit'>";
+
+        echo "</form>";
+
+        if (isset($_POST['submit'])) {
+            if (filter_var($_POST['year'], FILTER_VALIDATE_INT) &&
+                filter_var($_POST['runtime'], FILTER_VALIDATE_INT) &&
+                filter_var($_POST['image_url'], FILTER_VALIDATE_DOMAIN)) {
+                $title = $_POST['title'];
+                $runtime = $_POST['runtime'];
+                $year = $_POST['year'];
+                $director = $_POST['director'];
+                $produced = $_POST['country'];
+                $language = $_POST['language'];
+                $image_url = $_POST['image_url'];
+                }
+
+
+            $query = $db->prepare('INSERT INTO `films` (`Title`, `Running time`, `Year`, `Director`, `Produced`, `Language`, `image_url`) VALUES (:title, :runtime, :year, :director, :country, :language, :image_url)');
+
+            $query->bindParam(':title', $title);
+            $query->bindParam(':runtime', $runtime);
+            $query->bindParam(':year', $year);
+            $query->bindParam(':director', $director);
+            $query->bindParam(':country', $produced);
+            $query->bindParam(':language', $language);
+            $query->bindParam(':image_url', $image_url);
+
+            $query->execute();
         }
         ?>
     </div>
