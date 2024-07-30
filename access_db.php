d777c5cdec72d4a081f5fe0d067575bd9e77ca4d
Joseph Padfield <joseph.padfield@gmail.com>
2024-07-30 09:21:18 +0100

    added require_once to index and moved db query to new file

---
+++ access_db.php
@@ -0,0 +1,18 @@
+<?php
+$db = new PDO('mysql:host=db; dbname=my_films', 'root', 'password');
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare('SELECT * FROM `films`');
+$query->execute();
+
+$results = $query->fetchAll();
+
+foreach ($results as $film){
+    echo '<div class="item">';
+    echo '<p>' . $film['Title'] . '</p>';
+    echo '<p>' . $film['Director'] . '</p>';
+    echo '<p>' . $film['Year'] . '</p>';
+    echo '</div>';
+}
